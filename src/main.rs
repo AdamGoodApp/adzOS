d28d4e419befe50677cac9f800c24fcb65a556f7
Adam Rais <raisadamo@gmail.com>
2019-05-20 14:36:55 +0900

    Add custom cargo target by default

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@
 
 use core::panic::PanicInfo;
 
-// new entry point, linker looks for _start by default
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-  loop {}
-}
-
 /// this function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+  loop {}
+}
+
+// new entry point, linker looks for _start by default
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
   loop {}
 }
