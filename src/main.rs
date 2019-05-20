24d014f8b9834531bdccd7299996ee30aa904312
Adam Rais <raisadamo@gmail.com>
2019-05-20 15:15:08 +0900

    Add bootloader image

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,25 @@
 
 use core::panic::PanicInfo;
 
-/// this function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-  loop {}
-}
-
-static HELLO: &[u8] = b"Hello World!";
+static WELCOME_TEXT: &[u8] = b"Welcome to ADZ OS!";
 
 // new entry point, linker looks for _start by default
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   let vga_buffer = 0xb8000 as *mut u8;
 
-  for (i, &byte) in HELLO.iter().enumerate() {
+  for (i, &byte) in WELCOME_TEXT.iter().enumerate() {
     unsafe {
       *vga_buffer.offset(i as isize * 2) = byte;
       *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
     }
   }
 
+  loop {}
+}
+
+/// this function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
   loop {}
 }
