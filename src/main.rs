88cacc22ca11209bb35765a728fc666128160742
Adam Rais <raisadamo@gmail.com>
2019-06-03 16:57:21 +0900

    Adds a usable test environment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![no_std] // disable linking to the standard library
 #![no_main] // disable all Rust-level entry points
-
-use core::panic::PanicInfo;
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod power_manager;
+mod serial;
+
+use core::panic::PanicInfo;
+use power_manager::{QemuExitCode, exit_qemu};
 
 static WELCOME_TEXT: &str = "Welcome to ADZ OS!";
 
@@ -13,12 +19,37 @@ pub extern "C" fn _start() -> ! {
   println!("{}", WELCOME_TEXT);
   println!("version: 2.3");
 
+  #[cfg(test)]
+  test_main();
+
   loop{};
 }
 
 /// this function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    
+    for test in tests {
+      test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
 }
