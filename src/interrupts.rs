93a914b7dd294457e4fabd44f8b8ec69259bdaab
Adam Rais <raisadamo@gmail.com>
2019-06-06 12:44:13 +0900

    Add double fault handler to the IDT table

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -5,10 +5,14 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 #[cfg(test)]
 use crate::{serial_print, serial_println};
 
+// Create  new IDT table and set the handlers
 lazy_static! {
   static ref IDT: InterruptDescriptorTable = {
     let mut idt = InterruptDescriptorTable::new();
+
     idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.double_fault.set_handler_fn(double_fault_handler);
+
     idt
   };
 }
@@ -20,6 +24,11 @@ pub fn init_idt() {
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame)
 {
   println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64)
+{
+  panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,10 @@ pub extern "C" fn _start() -> ! {
 
   adz_os::init();
 
-  // invoke a breakpoint exception
-  x86_64::instructions::interrupts::int3();
+  // trigger a page fault
+  unsafe {
+    *(0xdeadbeef as *mut u64) = 42;
+  };
 
   #[cfg(test)]
   test_main();
